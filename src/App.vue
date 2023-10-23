206579e1b486ca1d72f778c77b6f8e767c341ee7
shreya shree <shreya.shr68@gmail.com>
2023-10-23 22:34:53 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <ImageUpload />
+  </div>
+</template>
+
+<script>
+import ImageUpload from './components/ImageUpload.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ImageUpload,
+  },
+};
+</script>
---
+++ src/components/ImageUpload.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <input type="file" @change="uploadImage" />
+    <img v-if="imageUrl" :src="imageUrl" alt="Uploaded Image" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      imageUrl: null,
+    };
+  },
+  methods: {
+    uploadImage(event) {
+      const file = event.target.files[0];
+      if (file) {
+        this.imageUrl = URL.createObjectURL(file);
+        // You can add additional logic here to upload the file to a server if needed.
+      }
+    },
+  },
+};
+</script>
